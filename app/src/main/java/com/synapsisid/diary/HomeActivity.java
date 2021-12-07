977f64ba01963ed4889b5c8c7afdf01830160461
zackmolt <fkrihkl@gmail.com>
2021-12-07 15:41:43 +0700

    initial commit

---
+++ app/src/main/java/com/synapsisid/diary/HomeActivity.java
@@ -0,0 +1,21 @@
+package com.synapsisid.diary;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.synapsisid.diary.databinding.ActivityHomeBinding;
+
+public class HomeActivity extends AppCompatActivity {
+
+    private ActivityHomeBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityHomeBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+
+    }
+}
